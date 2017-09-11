98d50873d36a62e9c361846087827b997d3da84c
Budimir49 <Budimir49@users.noreply.github.com>
2017-09-11 21:45:45 +0300

    sadf

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 $(function() {
-
     $('#my-menu').mmenu({
         extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
         navbar: {
@@ -16,6 +15,65 @@ $(function() {
     }).bind('closed', function () {
         $('.hamburger').removeClass('is-active')
     });
+
+    $('.carousel-services').on('initialized.owl.carousel', function() {
+        setTimeout(function () {
+            carouselService();
+        }, 100);
+    });
+
+    $('.carousel-services').owlCarousel({
+        // loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1100: {
+                items: 3
+            }
+        }
+    });
+
+
+
+    carouselService();
+
+    function carouselService()
+    {
+        $('.carousel-services-item').each(function () {
+            var ths = $(this);
+                thsh = ths.find('.carousel-services-content').outerHeight();
+                ths.find('.carousel-services-image').css('min-height', thsh);
+        })
+    }
+
+
+
+    $('.carousel-services-composition .h3').each(function()
+    {
+        var ths = $(this);
+        ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+    });
+
+
+    function onResize()
+    {
+        $('.carousel-services-content').equalHeights();
+    }
+    onResize();
+
+    
+    window.onResize = function ()
+    {
+        onResize();
+    }
 
 
 });
