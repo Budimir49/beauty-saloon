c1337b6037c2fdd1e8b6c6112d958f091ebc3be1
Budimir49 <Budimir49@users.noreply.github.com>
2017-09-23 20:41:33 +0300

    full complited

--- js/common.js
+++ js/common.js
@@ -52,6 +52,30 @@ $(function() {
     });
 
 
+    $('.parthers').owlCarousel({
+        loop: true,
+        items: 4,
+        smartSpeed: 1000,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
 
     carouselService();
 
@@ -80,6 +104,22 @@ $(function() {
 
     $('select').selectize();
 
+
+
+    $(window).scroll(function () {
+       if($(this).scrollTop() > $(this).height())
+       {
+            $('.top').addClass('active');
+       }
+       else
+       {
+           $('.top').removeClass('active');
+       }
+    });
+
+    $('.top').click(function () {
+       $('html,body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+    });
 
 
     //E-mail Ajax Send
@@ -120,4 +160,10 @@ $(function() {
 
 
 
+});
+
+
+
+$(window).on('load', function () {
+    $('.preloader').delay(1000).fadeOut('slow');
 });
